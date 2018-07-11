ded1a432eabe8f0e41d099003e87e38079a5a48d
Guilherme <guiachcar@hotmail.com>
2018-07-11 12:01:35 -0300

    mudanca de login

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@172.20.0.3/gstest?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@178.128.148.90:3306/gstest?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
